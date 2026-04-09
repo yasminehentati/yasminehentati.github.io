1355a050a03f61c5420fafa1603f395920e69e3a
yasminehentati <77170599+yasminehentati@users.noreply.github.com>
2026-04-09 22:35:06 +0000

    Deploying to gh-pages from @ yasminehentati/yasminehentati@a671a13c1386a0f4d8dcd2e00028afcdf164a1d0 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yasminehentati/website",
+    "data-repo": "yasminehentati/yasminehentati",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
