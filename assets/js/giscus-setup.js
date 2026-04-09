e491fa46536c5f842d6127ddc4ba0fc12551c526
yasminehentati <77170599+yasminehentati@users.noreply.github.com>
2026-04-09 22:49:03 +0000

    Deploying to gh-pages from @ yasminehentati/yasminehentati.github.io@09d60be0b056afb8f5c45daf93d77c887229c3a3 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "yasminehentati/yasminehentati",
+    "data-repo": "yasminehentati/yasminehentati.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
