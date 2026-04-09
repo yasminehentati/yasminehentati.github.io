331a63e874ee45f8d9c74839a0e5f28a7976f06a
yasminehentati <77170599+yasminehentati@users.noreply.github.com>
2026-04-09 23:27:11 +0000

    Deploying to gh-pages from @ yasminehentati/yasminehentati.github.io@9b86275aa4b35e77bcd457bc05c2cee1a33f14e7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -539,7 +539,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=nDnwGXIAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
